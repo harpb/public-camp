cc55cdebd81bc4af759c9774850ba9634fb08376
Harp <hbola@solarcity.com>
2018-09-23 13:36:13 -0700

    custom static root settings

--- camp/settings.py
+++ camp/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static_collection'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
